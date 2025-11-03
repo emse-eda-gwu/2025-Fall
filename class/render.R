93e9794d13d86137f679f8177d24b1e2e5f38a8f
John Helveston <john.helveston@gmail.com>
2025-11-03 18:28:25 -0500

    w11

--- class/render.R
+++ class/render.R
@@ -44,5 +44,5 @@ files13 <- c(
 # Create zip files of class notes
 zip::zip(
   zipfile = paste0(lesson, ".zip"),
-  files = c(files9, paste0(lesson, ".Rproj"))
+  files = c(files11, paste0(lesson, ".Rproj"))
 )
